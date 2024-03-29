e8fce47d7215e4666b1b3e651713e84c254adc0c
ColdWindScholar <3590361911@qq.com>
2024-03-29 23:52:56 +0800

    init

--- app/src/main/java/com/omarea/store/TimingTaskStorage.kt
+++ app/src/main/java/com/omarea/store/TimingTaskStorage.kt
@@ -5,15 +5,9 @@ import com.omarea.common.shared.ObjectStorage
 import com.omarea.model.TimingTaskInfo
 
 class TimingTaskStorage(private val context: Context) : ObjectStorage<TimingTaskInfo>(context) {
-    override fun load(configFile: String): TimingTaskInfo? {
-        return super.load(configFile)
-    }
 
     fun save(obj: TimingTaskInfo): Boolean {
         return super.save(obj, obj.taskId)
     }
 
-    override fun remove(configFile: String) {
-        super.remove(configFile)
-    }
 }
--- app/src/main/java/com/omarea/store/TriggerStorage.kt
+++ app/src/main/java/com/omarea/store/TriggerStorage.kt
@@ -5,15 +5,9 @@ import com.omarea.common.shared.ObjectStorage
 import com.omarea.model.TriggerInfo
 
 class TriggerStorage(private val context: Context) : ObjectStorage<TriggerInfo>(context) {
-    override fun load(configFile: String): TriggerInfo? {
-        return super.load(configFile)
-    }
 
     fun save(obj: TriggerInfo): Boolean {
         return super.save(obj, obj.id)
     }
 
-    override fun remove(configFile: String) {
-        super.remove(configFile)
-    }
 }
